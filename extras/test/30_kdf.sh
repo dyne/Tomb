6665fafe0f031fa8a0bed2f0983ecf929ac67188
Narrat <autumn-wind@web.de>
2024-07-28 20:39:08 +0200

    extras/tests: adjust 30_kdf for KDF related changes

--- extras/test/30_kdf.sh
+++ extras/test/30_kdf.sh
@@ -8,22 +8,22 @@ if test_have_prereq KDF; then
     test_export "kdf"
     test_expect_success 'Testing KDF: tomb creation' '
         tt_dig -s 20 &&
-        tt_forge --tomb-pwd $DUMMYPASS --kdf 1 &&
+        tt_forge --tomb-pwd $DUMMYPASS --kdf &&
         print $DUMMYPASS \
             | gpg --batch --passphrase-fd 0 --no-tty --no-options -d $tomb_key \
             | xxd &&
-        tt_lock --tomb-pwd $DUMMYPASS --kdf 1
+        tt_lock --tomb-pwd $DUMMYPASS
         '
 
     test_expect_success 'Testing KDF: tomb passwd' '
-        tt passwd -k $tomb_key --kdf 1 \
+        tt passwd -k $tomb_key --kdf \
             --unsafe --tomb-old-pwd $DUMMYPASS --tomb-pwd $DUMMYPASSNEW &&
-        tt passwd -k $tomb_key --kdf 1 \
+        tt passwd -k $tomb_key --kdf \
             --unsafe --tomb-old-pwd $DUMMYPASSNEW --tomb-pwd $DUMMYPASS
         '
 
     test_expect_success 'Testing KDF: tomb open & close' '
-        tt_open --tomb-pwd $DUMMYPASS --kdf 1 &&
+        tt_open --tomb-pwd $DUMMYPASS &&
         tt_close
         '
 fi
